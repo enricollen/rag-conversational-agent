52806966fba951ac4ceb9fa603c675b5dd890ba4
enrico <nello.enrico@gmail.com>
2024-07-05 19:31:28 +0200

    updated app.py and .env to handle openAPI llms

--- app.py
+++ app.py
@@ -1,24 +1,26 @@
 import argparse
 from flask import Flask, request, render_template, jsonify
-from llm import LLM
-from rag_retriever import RAGRetriever
+from llm.llm_factory import LLMFactory
+from retrieval.rag_retriever import RAGRetriever
 from dotenv import load_dotenv
 import os
 
-load_dotenv() 
+load_dotenv()
 
 CHROMA_PATH = os.getenv('CHROMA_PATH')
-LLM_MODEL_NAME = os.getenv('LLM_MODEL_NAME')
-EMBEDDING_MODEL_NAME = os.getenv('EMBEDDING_MODEL_NAME')
+LLM_MODEL_NAME = os.getenv('LLM_MODEL_NAME') # 'gpt-3.5-turbo', 'GPT-4o' or local LLM like 'llama3:8b'
+LLM_MODEL_TYPE = os.getenv('LLM_MODEL_TYPE')  # 'ollama' or 'gpt'
+EMBEDDING_MODEL_NAME = os.getenv('EMBEDDING_MODEL_NAME') # 'ollama' or 'openai'
 NUM_RELEVANT_DOCS = int(os.getenv('NUM_RELEVANT_DOCS'))
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
 app = Flask(__name__)
 
-# initialize the retriever
-retriever = RAGRetriever(chroma_path=CHROMA_PATH, embedding_model_name=EMBEDDING_MODEL_NAME)
+# Initialize the retriever
+retriever = RAGRetriever(chroma_path=CHROMA_PATH, embedding_model_name=EMBEDDING_MODEL_NAME, api_key=OPENAI_API_KEY)
 
-# choose the llm
-llm_model = LLM(model_name=LLM_MODEL_NAME)
+# Choose the LLM
+llm_model = LLMFactory.create_llm(model_type=LLM_MODEL_TYPE, model_name=LLM_MODEL_NAME, api_key=OPENAI_API_KEY)
 
 @app.route('/')
 def index():
@@ -34,7 +36,7 @@ def query():
     
     # Generate response from LLM
     llm_response = llm_model.generate_response(context=enhanced_context_text, question=query_text)
-    sources_html = "<br>".join(sources) 
+    sources_html = "<br>".join(sources)
     response_text = f"{llm_response}<br><br>Sources:<br>{sources_html}"
 
     return jsonify(response=response_text)
