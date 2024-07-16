f2b0bf5181bc40676801d5c5d4318872f98d4d25
enrico <nello.enrico@gmail.com>
2024-07-16 11:10:05 +0200

    updated test script for validating LLM responses

--- test_rag.py
+++ test_rag.py
@@ -6,12 +6,14 @@ from retrieval.rag_retriever import RAGRetriever
 
 load_dotenv()
 
-CHROMA_PATH = os.getenv('CHROMA_PATH')
+VECTOR_DB_OPENAI_PATH = os.getenv('VECTOR_DB_OPENAI_PATH')
+VECTOR_DB_OLLAMA_PATH = os.getenv('VECTOR_DB_OLLAMA_PATH')
 LLM_MODEL_NAME = os.getenv('LLM_MODEL_NAME')  # 'gpt-3.5-turbo', 'GPT-4o' or local LLM like 'llama3:8b'
-LLM_MODEL_TYPE = os.getenv('LLM_MODEL_TYPE')  # 'ollama' or 'gpt'
+LLM_MODEL_TYPE = os.getenv('LLM_MODEL_TYPE')  # 'ollama', 'gpt', 'claude'
 EMBEDDING_MODEL_NAME = os.getenv('EMBEDDING_MODEL_NAME')  # 'ollama' or 'openai'
 NUM_RELEVANT_DOCS = int(os.getenv('NUM_RELEVANT_DOCS'))
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+CLAUDE_API_KEY = os.getenv('CLAUDE_API_KEY')
 
 EVAL_PROMPT = """
 Expected Response: {expected_response}
@@ -20,9 +22,30 @@ Actual Response: {actual_response}
 (Answer with 'true' or 'false') Does the actual response match the expected response? 
 """
 
+def get_vector_db_path(embedding_model_name):
+    if embedding_model_name == "openai":
+        return VECTOR_DB_OPENAI_PATH
+    elif embedding_model_name == "ollama":
+        return VECTOR_DB_OLLAMA_PATH
+    else:
+        raise ValueError(f"Unsupported embedding model: {embedding_model_name}")
+
+def get_api_key(embedding_model_name):
+    if embedding_model_name == "openai":
+        return OPENAI_API_KEY
+    else:
+        return CLAUDE_API_KEY
+
 # Initialize the retriever and the LLM once
-retriever = RAGRetriever(chroma_path=CHROMA_PATH, embedding_model_name=EMBEDDING_MODEL_NAME, api_key=OPENAI_API_KEY)
-llm_model = LLMFactory.create_llm(model_type=LLM_MODEL_TYPE, model_name=LLM_MODEL_NAME, api_key=OPENAI_API_KEY)
+vector_db_path = get_vector_db_path(EMBEDDING_MODEL_NAME)
+api_key = get_api_key(EMBEDDING_MODEL_NAME)
+
+retriever = RAGRetriever(vector_db_path=vector_db_path, embedding_model_name=EMBEDDING_MODEL_NAME, api_key=api_key)
+llm_model = LLMFactory.create_llm(model_type=LLM_MODEL_TYPE, model_name=LLM_MODEL_NAME, api_key=api_key)
+
+print(LLM_MODEL_TYPE)
+print(LLM_MODEL_NAME)
+print(EMBEDDING_MODEL_NAME)
 
 def test_num_employees_alpha():
     assert query_and_validate(
@@ -68,17 +91,12 @@ def query_and_validate(question: str, expected_response: str, retriever, llm_mod
     # Generate response from LLM
     response_text = llm_model.generate_response(context=enhanced_context_text, question=question)
 
-    # Validation through LLM
-    if LLM_MODEL_TYPE == "ollama":
-        validation_model = OllamaModel(LLM_MODEL_NAME)
-    else:
-        validation_model = GPTModel(model_name=LLM_MODEL_NAME, api_key=OPENAI_API_KEY)
-
+    # Use the same LLM also for response validation
     prompt = EVAL_PROMPT.format(
         expected_response=expected_response, actual_response=response_text
     )
 
-    evaluation_results_str = validation_model.invoke(prompt)
+    evaluation_results_str = llm_model.invoke(prompt)
     evaluation_results_str_cleaned = evaluation_results_str.strip().lower()
 
     print(question)
@@ -98,6 +116,9 @@ def query_and_validate(question: str, expected_response: str, retriever, llm_mod
         )
 
 if __name__ == "__main__":
+    """
+        to run tests type: 'pytest test/test_rag.py -s' in cmd
+    """
     test_num_employees_alpha()
     test_company_field_beta()
     test_foundation_year_gamma()
